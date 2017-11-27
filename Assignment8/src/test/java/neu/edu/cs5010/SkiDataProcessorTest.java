14cb3bb17480d0dccb7443aca473fcddf28366b3
Song Zhe <songzhewhu@gmail.com>
2017-11-27 14:53:04 -0800

    Assignment8-Done

---
+++ Assignment8/src/test/java/neu/edu/cs5010/SkiDataProcessorTest.java
@@ -0,0 +1,24 @@
+package neu.edu.cs5010;
+
+import neu.edu.cs5010.dataProcess.SkiDataProcessor;
+import org.junit.Test;
+
+
+public class SkiDataProcessorTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void mainTest1() {
+        SkiDataProcessor.main(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void mainTest2(){
+        String[] s = new String[]{"abc"};
+        SkiDataProcessor.main(s);
+    }
+
+    @Test
+    public void mianTest2(){
+        String [] s = new String[]{"PDPAssignment.csv"};
+        SkiDataProcessor.main(s);
+    }
+}
