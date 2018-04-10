c83351168b6878809c631e08fa2cc13579908cdf
Hanyuxin <hanyuxinbnu@gmail.com>
2018-04-09 19:21:13 -0700

    change4200

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 
 //CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "https://cs5610-webdev-yuxinhan.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
